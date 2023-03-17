ca0159ae78c4233ce7000833eafa5cd99b2bf0f4
Jinhu Wang <jinhuwang1127@gmail.com>
2023-03-17 18:22:40 -0400

    init

---
+++ pages/components/layout/pages/water-flow/data.js
@@ -0,0 +1,47 @@
+const product = [{
+  image: 'https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/album/photo-7.jpg?sign=a09608b13d13402e657a8a37118f3190&t=1590025970',
+  title: '显瘦中长款系带风衣',
+  describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+  count: '888',
+  delCount: '666'
+},
+{
+  image: 'https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/album/photo-5.jpg?sign=785abbd12c4fa70454ef4bd7c3961536&t=1590025441',
+  title: '酵素洗纯棉四件套',
+  describe: '100%新疆精梳棉，柔软、厚实、光泽度好，颜色高级，2色可选颜色高级，2色可选,高级，2色可选',
+  count: '459',
+  delCount: '359'
+},
+{
+  image: 'https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/album/photo-4.jpg?sign=37d3fc32fe841a137e56b54cec2807c8&t=1590025434',
+  title: '环保棉做的男女款衬衫',
+  describe: '环保的高级长绒精梳棉，亲肤透气，安全无刺激，版型时髦有型，多款多色可选,环保的高级长绒精梳棉，亲肤透气，安全无刺激',
+  count: '888',
+  delCount: '666'
+},
+{
+  image: 'https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/album/photo-6.jpg?sign=6ba5b48f6766201a5458725defff47c7&t=1590025956',
+  title: '自带线的快充移动电源',
+  describe: '自带两条快充线，支持大部分快充设备充电，还能充笔记本电脑，有微电流模式,电脑，有微电流模式',
+  count: '888',
+  delCount: '666'
+},
+{
+  image: 'https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/album/photo-2.jpg?sign=7ccd10f793df154311f15a7b15d9ba57&t=1590025418',
+  title: '显瘦中长款系带风衣',
+  describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。不易皱好打理。',
+  count: '888',
+  delCount: '666'
+},
+{
+  image: 'https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/album/photo-5.jpg?sign=785abbd12c4fa70454ef4bd7c3961536&t=1590025441',
+  title: '酵素洗纯棉四件套',
+  describe: '100%新疆精梳棉，柔软、厚实、光泽度好，颜色高级，2色可选,100%新疆精梳棉，柔软、厚实、光泽度好，颜色高级，2色可选',
+  count: '459',
+  delCount: '359'
+}
+];
+
+export {
+  product
+};
